5f480bbaf2541d47a88415df3db700e17e9f160f
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-03-24 19:09:30 +0000

    containerize the server and create the docker compose stack

--- database/db.go
+++ database/db.go
@@ -1,11 +1,12 @@
 package database
 
 import (
-	_ "github.com/go-sql-driver/mysql"
 	"go-server/entities"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"log"
 )
 
 var db *gorm.DB
@@ -13,7 +14,7 @@ var db *gorm.DB
 func InitDB() {
 	var err error
 	//db, err = sql.Open("mysql", "user:password@tcp(localhost:8036)/db")
-	dsn := "user:password@tcp(localhost:8036)/db?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "user:password@tcp(database:3306)/db?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
